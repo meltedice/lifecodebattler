4ac74e0ca776eae15baf95c65c47571876150724
tacyan <tatsuharase@firstserver.co.jp>
2015-10-12 14:16:15 +0900

    add dummy mock

--- app/controllers/lifelogs_controller.rb
+++ app/controllers/lifelogs_controller.rb
@@ -5,8 +5,16 @@ class LifelogsController < ApplicationController
   def latest
     #最新のDBの情報の取得
     @app = Ncmb::DataStore.new("#{ENV["APP_KEY"]}", "#{ENV["CLIENT_KEY"]}", 'Log')
-    response = @app.where({user_id: params[:user_id]})
-    result = response["results"].last
+    response = @app.where({user_id: params[:user_id]})["results"]
+    steps = response.map { |v| v["steps"] }
+    distance_m = response.map { |v| v["distance_m"] }
+    usage_time_s = response.map { |v| v["usage_time_s"] }
+
+    sum_steps = steps.inject {|sum, n| sum + n }
+    sum_distance_m = distance_m.inject {|sum, n| sum + n }
+    sum_usage_time_s = usage_time_s.inject {|sum, n| sum + n }
+
+    result = {"steps": sum_steps,"distance_m": sum_distance_m ,"usage_time_s": sum_usage_time_s}
     render json: result
   end
 
